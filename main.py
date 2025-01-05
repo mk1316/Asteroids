42e9ccd61bf536fb0f75972f790677d283950315
mk1316 <kfesaha15@gmail.com>
2025-01-05 16:41:04 -0600

    player creation completed

--- main.py
+++ main.py
@@ -7,32 +7,40 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))     #set screen size
 
     clock = pygame.time.Clock()                                         #setup game clock
-    dt = 0
+
     
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    dt = 0
 
     while True:                                                         #Setting up game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+
+        for item in updateable:
+            item.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
-        player.update(dt)
+
+        
 
         dt = clock.tick(60) /1000                                       #framerate to 60 FPS
 
         
 
         
-
-
-
-
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
     main()
