54bafc0cb42ff80a4ae7e1d52b9c97568269619b
mk1316 <kfesaha15@gmail.com>
2025-01-05 23:53:16 -0600

    shooting ability

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation   = 0
+        self.rotation = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,3 +37,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
